4a81cf0123e1f860d26b98fd99767ae8f388dbb6
Irshaad Alha <23645370+nebuwan@users.noreply.github.com>
2025-04-13 00:12:20 -0400

    Testing API

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import requests
+
+baseurl = "https://rickandmortyapi.com/api/"
+
+endpoint = 'character'
+
+result = requests.get(baseurl + endpoint)
+data = result.json()
+
+pages = data['info']['pages']
+
+print(data['results'][0]['name'])
